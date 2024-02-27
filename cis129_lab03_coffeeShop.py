db74f7290eb34eac4667a2a8ab66f1b2b997c58d
mguadarrama96 <126596851+mguadarrama96@users.noreply.github.com>
2024-02-27 14:50:56 -0700

    Add files via upload

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,53 @@
+print('***************************************')
+
+print('My Coffee and Muffin Shop')
+
+print('Number of coffees bought?')
+
+coffees = int(input())
+
+print('Number of muffins bought?')
+
+muffins = int(input())
+
+print('***************************************')
+
+print()
+
+print()
+
+coffee_price = 5
+
+muffin_price = 4 
+
+subtotal = (coffees * coffee_price) + (muffins * muffin_price)
+
+coffee_subtotal = (coffees * coffee_price)
+
+fcoffee_subtotal = f'{coffee_subtotal: .2f}'
+
+muffin_subtotal = (muffins * muffin_price)
+
+fmuffin_subtotal = f'{muffin_subtotal: .2f}'
+
+tax_rate = .06
+
+tax = subtotal * tax_rate
+
+total = subtotal + tax
+
+print('***************************************')
+
+print('My Coffee and Muffin Shop Receipt') 
+
+print(coffees, 'Coffee at $' ,coffee_price, 'each: $' ,fcoffee_subtotal)
+
+print(muffins, 'Muffins at $' ,muffin_price, 'each: $' ,fmuffin_subtotal)
+
+print('6% tax: $' ,tax)
+
+print('----------')
+
+print('total: $' ,total)
+
+print('***************************************')
