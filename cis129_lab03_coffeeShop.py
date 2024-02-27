3908f1a3d44f5c73f847798778c1b92347f1a00f
mguadarrama96 <126596851+mguadarrama96@users.noreply.github.com>
2024-02-27 14:42:32 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,8 +1,12 @@
+print('***************************************')
 print('My Coffee and Muffin Shop')
 print('Number of coffees bought?')
 coffees = int(input())
 print('Number of muffins bought?')
 muffins = int(input())
+print('***************************************')
+print()
+print()
 coffee_price = 5
 muffin_price = 4 
 subtotal = (coffees * coffee_price) + (muffins * muffin_price)
@@ -13,9 +17,11 @@ fmuffin_subtotal = f'{muffin_subtotal: .2f}'
 tax_rate = .06
 tax = subtotal * tax_rate
 total = subtotal + tax
+print('***************************************')
 print('My Coffee and Muffin Shop Receipt') 
 print(coffees, 'Coffee at $' ,coffee_price, 'each: $' ,fcoffee_subtotal)
 print(muffins, 'Muffins at $' ,muffin_price, 'each: $' ,fmuffin_subtotal)
 print('6% tax: $' ,tax)
-print('-------------------------')
+print('----------')
 print('total: $' ,total)
+print('***************************************')
